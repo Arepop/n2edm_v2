7fa9b4bdcb951777d47cc5820c492f92b2cd4889
Popczak, Arkadiusz <arkadiusz.popczak@aptiv.com>
2020-12-21 14:31:12 +0100

    Updated Interfaces for objects

--- n2edm/abstract/objects.py
+++ n2edm/abstract/objects.py
@@ -13,25 +13,100 @@ class IObject(ABC):
         raise NotImplementedError("You need to implement 'name' property setter!")
 
     @abstractclassmethod
+    def create(cls):
+        raise NotImplementedError("You need to implement 'create' method!")        
+
+    @abstractclassmethod
+    def get(cls):
+        raise NotImplementedError("You need to implement 'get' method!")
+
+    @abstractclassmethod
+    def update(cls):
+        raise NotImplementedError("You need to implement 'update' method!")
+
+    @abstractclassmethod
     def delete(cls):
         raise NotImplementedError("You need to implement 'remove' method!")
-        
+
     @abstractclassmethod
-    def create(cls):
-        raise NotImplementedError("You need to implement 'create' method!")
+    def filter(cls):
+        raise NotImplementedError("You need to implement 'filter' method!")
 
-
-class IActorObject(IObject):
-    pass
-
-class IActionObject(IObject):
-    pass
 
 class IGroupObject(IObject):
-    pass
+    
+    @property
+    @abstractclassmethod
+    def children(self):
+        raise NotImplementedError("You need to implement 'children' getter method!")
+
+class IActionObject(IObject):
+
+    @property
+    @abstractclassmethod
+    def children(self):
+        raise NotImplementedError("You need to implement 'children' getter method!")
+
+    @property
+    @abstractclassmethod
+    def group(self):
+        raise NotImplementedError("You need to implement 'group' getter method!")
+
+    @group.setter
+    @abstractclassmethod
+    def group(self):
+        raise NotImplementedError("You need to implement 'setter' getter method!")
+
+class IActorObject(IObject):
+    @property
+    @abstractclassmethod
+    def group(self):
+        raise NotImplementedError("You need to implement 'group' getter method!")
+
+    @group.setter
+    @abstractclassmethod
+    def group(self):
+        raise NotImplementedError("You need to implement 'group' setter method!")
+
+    @property
+    @abstractclassmethod
+    def action(self):
+        raise NotImplementedError("You need to implement 'action' getter method!")
+
+    @action.setter
+    @abstractclassmethod
+    def action(self):
+        raise NotImplementedError("You need to implement 'action' setter method!")
 
 class ITimelineObject(IActorObject):
-    pass
+    @property
+    @abstractclassmethod
+    def action(self):
+        raise NotImplementedError("You need to implement 'action' getter method!")
+
+    @action.setter
+    @abstractclassmethod
+    def action(self):
+        raise NotImplementedError("You need to implement 'action' setter method!")
+
 
 class IInfinitActorObject(IActorObject):
-    pass
+    @property
+    @abstractclassmethod
+    def group(self):
+        raise NotImplementedError("You need to implement 'group' getter method!")
+
+    @group.setter
+    @abstractclassmethod
+    def group(self):
+        raise NotImplementedError("You need to implement 'group' setter method!")
+
+    @property
+    @abstractclassmethod
+    def action(self):
+        raise NotImplementedError("You need to implement 'action' getter method!")
+
+    @action.setter
+    @abstractclassmethod
+    def action(self):
+        raise NotImplementedError("You need to implement 'action' setter method!")
