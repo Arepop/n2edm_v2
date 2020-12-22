ae6e00226e22c0ebe516476a105546ccf8f9d891
Popczak, Arkadiusz <arkadiusz.popczak@aptiv.com>
2020-12-22 15:19:10 +0100

    TODO: New feature to auto-create models when creating objects

--- n2edm/scheduler/main.py
+++ n2edm/scheduler/main.py
@@ -9,9 +9,18 @@ def main():
 
     from ..core.objects import GroupObject, ActionObject
 
-    group_model_object = GroupObject.model.objects.get(pk=1)
-    fields = {field.name: field.value for field in group_model_object._meta.get_fields()}
-    print(fields)
+    group = GroupObject.model.objects.get(pk=1)
+    fields = {}
+    for field in group._meta.get_fields():
+        if field.name == 'action':
+            continue
+        elif field.name == "id":
+            fields['id_'] = field.value_from_object(group)
+        else:
+            fields[field.name] = field.value_from_object(group)
+
+    GroupObject.create(**fields)
+    print(GroupObject.get(id_=1))
 
 if __name__ == "__main__":
     main()
