77767e7bf968b0cdd2a395106da9b961155f0f0b
juliusz <juliusz.brzeziak@student.uj.edu.pl>
2021-02-10 21:00:50 +0100

    check_unique in Handle

--- n2edm/core/handlers.py
+++ n2edm/core/handlers.py
@@ -3,27 +3,31 @@ from .objects import *
 
 
 class Handler(IHandler):
-    pass
+
+    object_ = Object
+
+    def __init__(self):
+        self.no_object = 0
+
+    @classmethod
+    def check_unique(cls, name, set_id):
+        names = [cls.object_.name for obj in cls.object_.filter(set_id=set_id)]
+        if name in names:
+            raise NameError(
+                "Object with that name alredy exist in that set. Choose different name"
+            )
 
 
 class GroupHandler(Handler, IGroupHandler):
-    def __init__(self):
-        self.no_groups = 0
 
-    def check_unique(self, name, set_id):
-        names = [group.name for group in GroupObject.filter(set_id=set_id)]
-        if name in names:
-            raise NameError(
-                "GroupObject with that name alredy exist in that set. Choose different name"
-            )
-
-    # def set_position(self):
-    #     for
+    pass
 
 
 class ActionHandler(Handler, IActionHandler):
+
     pass
 
 
 class ActorHandler(Handler, IActorHandler):
+
     pass
